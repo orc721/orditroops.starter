c8d5e91b5006fbfe4b99068c78428bb3f548c19f
Gerald Bauer <gerald.bauer@gmail.com>
2023-07-09 12:53:16 +0200

    up

--- sandbox/fam.rb
+++ sandbox/fam.rb
@@ -16,9 +16,25 @@ recs = recs[0, max]
 puts "   #{recs.size} record(s)"
 
 
+## (lookup) mapping (table) ord num to ord id
+mapping = read_csv( "./sandbox/diyorditroops_ids.csv" ).reduce( {} ) do |mapping, rec|
+                                         mapping[rec['num']] = rec['id']
+                                         mapping
+                                    end
+
 
 recs.each_with_index do |rec,i|
-  print %Q[<img src="../num/#{rec['num']}.png" alt="Trooper ##{i} - Inscribe № #{rec['num']}" width="10%">]
+  num = rec['num']
+  id  = mapping[ num ]
+  if id.nil?
+    puts "!! error - no inscribe num to inscribe id mapping found for:"
+    pp rec
+    exit 1
+  end
+
+  print %Q[<a href="https://ordinals.com/inscription/#{id}" title="Trooper ##{i} - Inscribe № #{num}">]
+  print %Q[<img src="../num/#{num}.png" width="10%">]
+  print %Q[</a>]
 end
 
 
